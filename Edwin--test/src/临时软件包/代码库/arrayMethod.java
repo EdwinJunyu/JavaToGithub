f1be08c7d72a9079099bd6f9b249172b78bf7358
Edwin <534694474@qq.com>
2023-01-31 00:52:07 +0800

    Array工具类&冒泡排序实战

---
+++ Edwin--test/src/临时软件包/代码库/arrayMethod.java
@@ -0,0 +1,19 @@
+package 临时软件包.代码库;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class arrayMethod {
+    public static void main(String[] args) {
+        //toString 方法可以直接答应数组（不需要for遍历）
+        int[] arr = new int[]{1,4,9,11,23,50,21,6,8,3,5,6,2};
+        System.out.println(Arrays.toString(arr));  //直接sout
+
+        Arrays.sort(arr);  //sort 排序
+        System.out.println(Arrays.toString(arr));
+
+        int[] array = new int[10];
+        Arrays.fill(array,66);  //快速填充数组
+        System.out.println(Arrays.toString(array));
+    }
+}
