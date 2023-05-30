fffa97dfd99da9c45eafd5600c6a85d8f83aa120
Edwin <534694474@qq.com>
2023-05-30 14:46:43 +0800

    没改什么...

--- Edwin--test/src/JavaWeb/Socket/客户端.java
+++ Edwin--test/src/JavaWeb/Socket/客户端.java
@@ -1,4 +1,4 @@
-package JavaWeb;
+package JavaWeb.Socket;
 
 import java.io.*;
 import java.net.Socket;
--- Edwin--test/src/JavaWeb/Socket/服务器.java
+++ Edwin--test/src/JavaWeb/Socket/服务器.java
@@ -1,4 +1,4 @@
-package JavaWeb;
+package JavaWeb.Socket;
 
 import java.io.BufferedReader;
 import java.io.IOException;
