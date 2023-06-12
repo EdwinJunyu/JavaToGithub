6e26b4ee027f1c2bdf37b3892913c5c72a142f3d
Edwin <534694474@qq.com>
2023-06-12 10:44:44 +0800

    mybatis 增删改查

--- src/main/java/mybatis/userMapper.java
+++ src/main/java/mybatis/userMapper.java
@@ -1,8 +1,31 @@
 package mybatis;
 
+import java.util.List;
+
 public interface userMapper {
     /**
      * 添加用户信息
      */
     int insertUser();
+
+    /**
+     * 修改用户信息
+     */
+    void updateUser();
+
+    /**
+     * 删除用户信息
+     */
+    void deleteUser();
+
+    /**
+     * 根据id查询用户信息
+     */
+    user getUserById();
+
+    /**
+     * 查询所有用户信息
+     */
+    List<user> getAllUser();
+
 }
--- src/test/java/mybatisTest.java
+++ src/test/java/mybatisTest.java
@@ -1,3 +1,4 @@
+import mybatis.user;
 import mybatis.userMapper;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class mybatisTest {
     @Test
@@ -18,14 +20,26 @@ public class mybatisTest {
         //获取SqlSessionFactory
         SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
         //获取SqlSession
-        SqlSession sqlSession = sqlSessionFactory.openSession();
+        SqlSession sqlSession = sqlSessionFactory.openSession(true); //true为自动提交
         //获取mapper接口对象
-        userMapper mapper =  sqlSession.getMapper(userMapper.class);
+        userMapper mapper = sqlSession.getMapper(userMapper.class);
         //测试功能
-        mapper.insertUser();
+        //提交事务
+        //sqlSession.commit();
         int result = mapper.insertUser();
         System.out.println("result:" + result);
-
-
+    }
+    @Test
+    public void testCRUD() throws IOException{
+        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        userMapper mapper = sqlSession.getMapper(userMapper.class);
+        //mapper.updateUser();
+        //mapper.deleteUser();
+        //user user = mapper.getUserById();
+        //System.out.println(user);
+        List<user> list = mapper.getAllUser();
+        list.forEach(user -> System.out.println(user));
     }
 }
